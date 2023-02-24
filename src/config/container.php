3a4c3a655aae720235f39fdd030efe5fe7d8fe37
vitaliy <zndev@ya.ru>
2023-02-24 23:17:06 +0600

    upd

--- src/config/container.php
+++ src/config/container.php
@@ -1,14 +1,11 @@
 <?php
 
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Untek\Core\Container\Interfaces\ContainerConfiguratorInterface;
-use Untek\Core\EventDispatcher\Interfaces\EventDispatcherConfiguratorInterface;
-use Untek\Core\EventDispatcher\Libs\EventDispatcherConfigurator;
 
 return function (ContainerConfiguratorInterface $containerConfigurator) {
-    $containerConfigurator->singleton(EventDispatcherConfiguratorInterface::class, EventDispatcherConfigurator::class);
-    $containerConfigurator->singleton(EventDispatcherInterface::class, EventDispatcher::class);
+    $containerConfigurator->singleton(\Untek\Core\EventDispatcher\Interfaces\EventDispatcherConfiguratorInterface::class, \Untek\Core\EventDispatcher\Libs\EventDispatcherConfigurator::class);
+    $containerConfigurator->singleton(\Symfony\Contracts\EventDispatcher\EventDispatcherInterface::class, EventDispatcher::class);
     $containerConfigurator->singleton(\Symfony\Component\EventDispatcher\EventDispatcherInterface::class, EventDispatcher::class);
-    $containerConfigurator->singleton(\Psr\EventDispatcher\EventDispatcherInterface::class, EventDispatcherInterface::class);
+    $containerConfigurator->singleton(\Psr\EventDispatcher\EventDispatcherInterface::class, EventDispatcher::class);
 };
