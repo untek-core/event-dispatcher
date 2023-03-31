57d1f94a1e3a8261712214a2379361b693ec447a
vitaliy <zndev@ya.ru>
2023-03-31 10:30:12 +0600

    upd

---
+++ src/Infrastructure/DependencyInjection/Symfony/services/event-dispatcher.php
@@ -0,0 +1,16 @@
+<?php
+
+use Psr\EventDispatcher\EventDispatcherInterface as PsrEventDispatcherInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface as SymfonyComponentEventDispatcherInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface as SymfonyContractEventDispatcherInterface;
+
+return static function (ContainerConfigurator $configurator): void {
+    $services = $configurator->services();
+
+    $services->set(EventDispatcher::class, EventDispatcher::class);
+    $services->alias(SymfonyComponentEventDispatcherInterface::class, EventDispatcher::class);
+    $services->alias(SymfonyContractEventDispatcherInterface::class, EventDispatcher::class);
+    $services->alias(PsrEventDispatcherInterface::class, EventDispatcher::class);
+};
